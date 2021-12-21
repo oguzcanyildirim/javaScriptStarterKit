6d9713dd3d5cae25b6d11faed22618fdb968478a
Oguzcan Yildirim <oguzcanyildirim91@gmail.com>
2021-12-21 11:03:19 +0100

    lesson 2: function definition types

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,20 @@
-function addToCart(productName) {
-    console.log("Sepete Eklendi : " + productName)
+function addToCart(quantity, productName="Elma") {
+    console.log("Sepete Eklendi : ürün: " + productName + " adet " + quantity)
 }
+
+addToCart()
+addToCart(5, "Limon")
+addToCart(10, "Yumurta")
+addToCart(1, "Karpuz")
+
+let sayHello = ()=>{
+    console.log("Hello World")
+}
+
+sayHello()
+
+let sayHello2 = function () {
+    console.log("Hello World 2")
+}
+
+sayHello2()

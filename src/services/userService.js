486de6fe638475d2d5fbf1a1e5c67492b3943a42
Oguzcan Yildirim <oguzcanyildirim91@gmail.com>
2021-12-24 17:41:35 +0100

    oop with js

---
+++ src/services/userService.js
@@ -0,0 +1,13 @@
+export default class UserService{
+    add(){
+        console.log("user added")
+    }
+
+    list(){
+        console.log("users listed")
+    }
+
+    getById(id){
+        console.log("user retrieved")
+    }
+}
